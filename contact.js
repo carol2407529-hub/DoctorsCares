e1a3644f7903b315daf72c3ba4f7de8207b3582b
carol2407529-hub <carol2407529@miuegypt.edu.eg>
2026-06-08 00:59:24 +0300

    Delete contact.js

--- contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { contact } = require('../controllers/contact.controller')
-const router = express.Router();
-
-router.post('/contact', contact);
-
-module.exports = router;
